196a9389e0c630a9a3b3e2be758cc8d93810ff20
José Eduardo Díaz Morales <jdiazm1701@alumno.ipn.mx>
2025-05-20 18:43:04 -0600

    day-03

---
+++ Day-03/tokens.py
@@ -0,0 +1,7 @@
+import nltk
+nltk.download('punkt_tab')
+from nltk.tokenize import word_tokenize
+sample_text = 'I love programming!'
+tokens = word_tokenize(sample_text)
+
+print('Tokens:', tokens)
